866c2b472df3666ad406a6bd4b7fc7a4d6e83ab7
Galaad Linosfil <neferetheka@gmail.com>
2012-03-21 18:42:04 +0100

    Update HTMLHelper.java

--- HTMLHelper.java
+++ HTMLHelper.java
@@ -1,12 +1,12 @@
 public abstract class HTMLHelper
 {
 	/*
-	* Permit to convert hhtml text to plain text
+	* Permit to convert html text to plain text
 	*/
-	public static String convertFromHTML(String chaine)
+	public static String convertFromHTML(String chaine, boolean isSpecial)
 	{
-		return chaine.replace("&eacute", "é").replace("&egrave", "è").replace("&agrave", "à").replace("&eacute", "é")
-				.replace("&egrave", "è").replace("&agrave", "à").replace("&ecirc", "ê").replace("&ocirc", "ô")
-				.replace("&ucirc", "û").replace("&ugrave", "ù").replace("&ccedil", "ç").replace("&acirc", "â");
+		return chaine.replace("&eacute;", "Ã©").replace("&egrave;", "Ã¨").replace("&agrave;", "Ã ")
+				.replace("&ecirc;", "Ãª").replace("&ocirc;", "Ã´")
+				.replace("&ucirc;", "Ã»").replace("&ugrave;", "Ã¹").replace("&ccedil;", "Ã§").replace("&acirc;", "Ã¢");
 	}
 }
