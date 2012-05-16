eb06ce443b23fa66bf1684c14136ac5106470ff5
Galaad Linosfil <neferetheka@gmail.com>
2012-05-16 16:13:06 +0300

    add the pluralize method

--- Strings.java
+++ Strings.java
@@ -1,11 +1,19 @@
 public abstract class Strings
 {
 	/*
-	*	Universal method for detect a null or empty string
+	* Universal method to detect a null or empty string
 	* Better than isEmpty() because it works on older Java system and apply trim (the original method doesn't)
 	*/
 	public static boolean isNullOrEmpty(String string)
 	{
 		return string == null || string.trim().length() == 0; 												
+	}
+	
+	/*
+	* Permits to pluralize a String depending on a number
+	*/
+	public static String pluralize(String string, int count)
+	{
+		return (count == 0 || count == 1) ? string+"s" : string;											
 	}
 }
