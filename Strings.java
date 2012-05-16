9f8568e8cbd04439b04fa1340e7fe41f22b849df
Galaad Linosfil <neferetheka@gmail.com>
2012-05-16 19:16:54 +0300

    Optimisation for pluralize method Add ucfirst and mergeArray methods

--- Strings.java
+++ Strings.java
@@ -14,6 +14,29 @@ public abstract class Strings
 	*/
 	public static String pluralize(String string, int count)
 	{
-		return (count == 0 || count == 1) ? string+"s" : string;											
+		return (count <= 1) ? string+"s" : string;											
+	}
+	
+	/*
+	 * Clone of the ucfirst function in PHP
+	 * Transforms the first letter of the string in uppercase
+	 */
+	public static String ucfirst(String string)
+	{
+		return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+	}
+	
+	/*
+	 * Merges an string array into a single string with the specified separator
+	 */
+	public static String mergeArray(String[] strings, String separator)
+	{
+		String result = "";
+		for (String string : strings)
+		{
+			result += string + separator;
+		}
+		result = result.substring(0, result.length() - 2);
+		return result;
 	}
 }
