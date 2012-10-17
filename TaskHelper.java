6679e3137091d95824a9679716eeeba308fcb4a8
Galaad Linosfil <neferetheka@gmail.com>
2012-10-17 12:01:56 +0200

    Replace Activity by Context in tasks

--- TaskHelper.java
+++ TaskHelper.java
@@ -8,23 +8,23 @@ public abstract class TaskHelper
 	/*
 	 * Permit to launch a share intent
 	 */
-	public static void shareTask(Activity activity, String title, String content)
+	public static void shareTask(Context context, String title, String content)
 	{
 		Intent intent = new Intent(android.content.Intent.ACTION_SEND);
 		intent.setType("text/plain");
 		intent.putExtra(Intent.EXTRA_SUBJECT, title);
 		intent.putExtra(Intent.EXTRA_TEXT, content
 				+ " - via MyApp");
-		activity.startActivity(Intent.createChooser(intent, title));
+		context.startActivity(Intent.createChooser(intent, title));
 	}
 
 	/*
 	 * Permit to launch a webbrowser intent
 	 */
-	public static void browserTask(Activity activity, String lien)
+	public static void browserTask(Context context, String link)
 	{
-		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(lien));
-		activity.startActivity(intent);	
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+		context.startActivity(intent);	
 	}
 	
 	/*
