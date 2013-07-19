d7e4796dfc16a2f7b8be3b11d79c2c4378ba14a2
Galaad Linosfil <neferetheka@gmail.com>
2013-07-19 15:21:08 +0200

    Fix Resources import

--- src/com/aerilys/helpers/android/UIHelper.java
+++ src/com/aerilys/helpers/android/UIHelper.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.DisplayMetrics;
