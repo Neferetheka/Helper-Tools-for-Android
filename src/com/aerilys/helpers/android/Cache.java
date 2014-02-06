e9af1194069ed86d6c72e4c3e01bd65a09802a7b
Galaad Linosfil <neferetheka@gmail.com>
2014-02-06 22:34:36 +0100

    Much better cache class

--- src/com/aerilys/helpers/android/Cache.java
+++ src/com/aerilys/helpers/android/Cache.java
@@ -1,57 +1,69 @@
 package com.aerilys.helpers.android;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-@SuppressLint("SimpleDateFormat")
 public final class Cache
 {
 	private static SharedPreferences prefs;
 
+	/**
+	 * Get an item from the cache
+	 * @param context : app context
+	 * @param key : key of the item we want to retrieve
+	 * @return : request string from cache, or null
+	 */
 	public static String getItem(Context context, String key)
 	{
 		getPrefs(context);
-		if(prefs.contains(key+"cache"))
+		if (prefs.contains(key + "cache"))
 		{
-			try
+			long expirationTime = prefs.getLong(key + "cache", System.currentTimeMillis());
+			if (expirationTime < System.currentTimeMillis())
 			{
-				Date date = new SimpleDateFormat().parse((prefs.getString(key+"cache", null)));
-				if(date.before(new Date()))
-				{
-					SharedPreferences.Editor editor = prefs.edit();
-					editor.remove(key);
-					editor.remove(key+"cache");
-					editor.commit();
-					return null;
-				}
-			}
-			catch (ParseException e)
-			{
-				e.printStackTrace();
+				SharedPreferences.Editor editor = prefs.edit();
+				editor.remove(key);
+				editor.remove(key + "cache");
+				editor.commit();
 				return null;
 			}
 		}
-		
+
 		return prefs.getString(key, null);
 	}
 
-	public static boolean setitem(Context context, String key, String item, Date toExpire)
+	/**
+	 * Set an item in cache with an expiration date.
+	 * 
+	 * @param context
+	 *            : app context
+	 * @param key
+	 *            : cache key
+	 * @param item
+	 *            : item to put in cache
+	 * @param timeBeforeExpiration
+	 *            : time before expiration in milliseconds.
+	 * @return : a success boolean
+	 */
+	public static boolean setitem(Context context, String key, String item, long timeBeforeExpiration)
 	{
 		getPrefs(context);
 		SharedPreferences.Editor editor = prefs.edit();
 		editor.putString(key, item);
-		SimpleDateFormat format = new SimpleDateFormat();
-		editor.putString(key+"cache", format.format(toExpire));
+		long expirationTime = System.currentTimeMillis() + timeBeforeExpiration;
+		editor.putLong(key + "cache", expirationTime);
 		return editor.commit();
 	}
 
+	/**
+	 * Get cache preferences
+	 * @param context : app context
+	 */
 	private static void getPrefs(Context context)
 	{
 		if (prefs == null)
+		{
 			prefs = context.getSharedPreferences("cache", Context.MODE_PRIVATE);
+		}
 	}
 }
