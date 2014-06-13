4be81cbd243810896884ceefc4c793aa0b911646
Galaad Linosfil <neferetheka@gmail.com>
2014-06-13 18:47:39 +0200

    Huge RandomExtension update

--- src/com/aerilys/helpers/android/RandomExtension.java
+++ src/com/aerilys/helpers/android/RandomExtension.java
@@ -1,25 +1,67 @@
 package com.aerilys.helpers.android;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class RandomExtension extends Random
+public abstract class RandomExtension extends Random
 {
 	private static final long serialVersionUID = 8914228484090011478L;
 
+	public static int sNextInt(int max)
+	{
+		return (int)(Math.random()*(max));
+	}
+	
 	/*
 	* Extension method of nextInt and able to generate a random number between two numbers
 	*/
-	public int nextInt(int min, int max)
+	public static int nextInt(int min, int max)
 	{
 		return min + (int)(Math.random()*(max - min));
 	}
 	
-	public String generateRandomString(int length)
+	public static String generateRandomString(int length)
 	{
 		String alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 		StringBuilder sb = new StringBuilder(length);
 		for (int i = 0; i < length; i++)
-			sb.append(alphabet.charAt(nextInt(alphabet.length())));
+			sb.append(alphabet.charAt(RandomExtension.sNextInt(alphabet.length())));
 		return sb.toString();
+	}
+	
+	public static <T> T getRandomElementOfList(List<T> list)
+	{
+		if(list == null || list.isEmpty())
+		{
+			return null;
+		}
+		return list.get(nextInt(0, list.size()-1));
+	}
+	
+	public static <T> T getRandomElementOfList(List<T>... lists)
+	{
+		List<T> mergedList = new ArrayList<T>();
+		for(List<T> list : lists)
+		{
+			mergedList.addAll(list);
+		}
+		
+		if(mergedList.size() == 0)
+		{
+			return null;
+		}
+		
+		return mergedList.get(nextInt(0, mergedList.size()-1));
+	}
+	
+	public static boolean getRandomBoolean()
+	{
+		if(nextInt(0, 1) == 1)
+		{
+			return true;
+		}
+		
+		return false;
 	}
 }
